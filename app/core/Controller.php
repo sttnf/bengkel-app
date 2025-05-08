82315b18691d8063957d3162396cb151ff3686be
Rafa Al Razzak <rafaar@kita.blue>
2025-05-08 07:09:01 +0700

    feat: update Controller methods to include type hints and improve documentation

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -4,18 +4,23 @@ namespace App\Core;
 use JetBrains\PhpStorm\NoReturn;
 
 abstract class Controller {
+    /**
+     * @throws \Exception
+     */
     protected function render($view, $data = []): string
     {
         $router = new Router();
         return $router->renderView($view, $data);
     }
 
-    #[NoReturn] protected function redirect($url) {
+    #[NoReturn] protected function redirect(string $url): void
+    {
         header("Location: $url");
         exit;
     }
 
-    #[NoReturn] protected function json($data) {
+    #[NoReturn] protected function json($data): void
+    {
         header('Content-Type: application/json');
         echo json_encode($data);
         exit;
